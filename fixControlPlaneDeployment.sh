6ce2f7773ca2e4522320b8bf4cdebac77e109b3f
Jose Mari C. Wong <71325113+JMWong1599@users.noreply.github.com>
2023-09-14 09:30:14 +0800

    Update fixControlPlaneDeployment.sh

--- fixControlPlaneDeployment.sh
+++ fixControlPlaneDeployment.sh
@@ -9,4 +9,7 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 # Change the ownership of the copied config file to the user
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+#add Calico object to kubesystem to ready all nodes 
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
 echo "Control plane deployment fixed!"
