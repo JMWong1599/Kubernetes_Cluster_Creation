1d46306181eefe2fa5b914d66082e24cadae3466
Jose Mari C. Wong <71325113+JMWong1599@users.noreply.github.com>
2024-03-27 14:14:58 +0800

    Update surely setup.sh

--- setup.sh
+++ setup.sh
@@ -13,13 +13,19 @@ sudo apt-get install -y docker.io
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 
 # Add Kubernetes GPG key
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 # Add Kubernetes repository
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Install Kubernetes packages
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Enable the kubelet service before running kubeadm-Optional
+sudo systemctl enable --now kubelet
 
 echo "Setup complete!"
