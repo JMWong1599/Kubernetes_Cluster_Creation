134a1176ff5a3f8a4ecc756b2dac8b76712d5f9d
Jose Mari C. Wong <71325113+JMWong1599@users.noreply.github.com>
2024-03-27 14:30:48 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
+# Set-up for Individual VMs of a Kubernetes Cluster 
 
 # Update package list
-sudo apt-get update
+sudo apt update
 
-# Disable swap
-sudo swapoff -a
+# Install necessary packages
+sudo apt install -y nano net-tools openssh-server
+
+# Enable and allow SSH
+sudo systemctl enable ssh
+sudo ufw allow ssh
+sudo systemctl start ssh
 
 # Install Docker
-sudo apt-get install -y docker.io
+sudo apt install docker.io -y
+
+# Install additional packages for Kubernetes
+sudo apt install apt-transport-https curl -y
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+# Update package list again
+sudo apt update
 
 # Install Kubernetes components
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-
-# Add Kubernetes GPG key
-# sudo mkdir -p -m 755 /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
-# Add Kubernetes repository
-# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-# Install Kubernetes packages
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-
-# Enable the kubelet service before running kubeadm-Optional
-sudo systemctl enable --now kubelet
+sudo apt install kubeadm kubelet kubectl kubernetes-cni -y
 
 echo "Setup complete!"
